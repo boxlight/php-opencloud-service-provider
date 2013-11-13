8e64b967451ef3b7e04b2c100fba7b1ae7487e21
Robert Cambridge <robert@boxlightmedia.com>
2013-11-13 12:10:46 +0000

    implementation of a working service provider

--- src/Boxlight/Silex/Provider/OpenCloud/OpenCloudServiceProvider.php
+++ src/Boxlight/Silex/Provider/OpenCloud/OpenCloudServiceProvider.php
@@ -30,7 +30,36 @@ class OpenCloudServiceProvider implements ServiceProviderInterface
 
             $fqcn = sprintf('OpenCloud\%s', $variant);
 
-            return new $fqcn($app['opencloud.endpoint'], $app['opencloud.credentials'], $app['opencloud.options']);
+            $opencloud = new $fqcn($app['opencloud.endpoint'], $app['opencloud.secret'], isset($app['opencloud.options']) ? $app['opencloud.options'] : array());
+
+            if (isset($app['opencloud.importCredentials'])) {
+
+                if (is_callable($app['opencloud.importCredentials'])) {
+                    $credentials = call_user_func($app['opencloud.importCredentials']);
+                } else {
+                    $credentials = $app['opencloud.importCredentials'];
+                }
+
+                // the result may still be falsy because the callable may return null
+                // take that to mean that there are no credentials cached
+                if ($credentials) {
+                    $opencloud->importCredentials($credentials);
+                }
+            }
+
+            $opencloud->authenticate();
+
+            $credentials = $opencloud->exportCredentials();
+
+            if (isset($app['opencloud.exportCredentials'])) {
+                if (is_callable($app['opencloud.exportCredentials'])) {
+                    call_user_func($app['opencloud.exportCredentials'], $credentials);
+                }
+            } else {
+                $app['opencloud.exportCredentials'] = $credentials;
+            }
+
+            return $opencloud;
         });
     }
 }
